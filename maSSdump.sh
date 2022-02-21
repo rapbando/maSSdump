b56bf488195878dd3bee6f819e476156b96dfb60
rapbando <rapbando@hotmail.it>
2022-02-21 21:08:37 +0000

    Add files via upload

---
+++ maSSdump.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+ masscan -p443 $1 -oG session.massdump  --rate=1000   
+  if [[ $? -ne 0 ]] ; then
+      exit 1
+  fi
+ grep -oP '(?<=Host: )\S*' session.massdump >openports.massdump 
+ printf "[+] Looking up the certificate CNs for available hosts.....\n---------------------------------------------------------------------\n"
+ echo
+ ./cndump -f openports.massdump |& tee out.massdump.txt
+ rm session.massdump openports.massdump
+ printf "\n---------------------------------------------------------------------\nSaved  domains to: out.massdump.txt\n" 
+ echo -n "FINISH!!!"
+ echo          
+
+
