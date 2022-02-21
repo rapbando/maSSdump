630065002c17b05b4203945fd7fdb246927cf961
rapbando <rapbando@hotmail.it>
2022-02-21 22:08:58 +0000

    Update maSSdump.sh

--- maSSdump.sh
+++ maSSdump.sh
@@ -8,7 +8,7 @@
  grep -oP '(?<=Host: )\S*' session.massdump >openports.massdump 
  printf "[+] Looking up the certificate CNs for available hosts.....\n---------------------------------------------------------------------\n"
  echo
- ./cndump -f openports.massdump |& tee out.massdump.txt
+ ./cndump -f openports.massdump -u |& tee out.massdump.txt
  rm session.massdump openports.massdump
  printf "\n---------------------------------------------------------------------\nSaved  domains to: out.massdump.txt\n" 
  echo -n "FINISH!!!"
